ae1f40a2c956787cfe3e6460f2cb71c72f1b11cd
chancellor carlton <chancellorcarlton@chancellors-MacBook-Pro.local>
2022-01-12 09:59:15 -0700

    Third Commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { append } = require('express/lib/response');
+const { append } = require('express/lib/response'); //this line was automatically added.
 const path = require('path');
 
 const app = express();
@@ -7,6 +7,8 @@ const app = express();
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+app.use(express.static('public')) //linking public folder
 
 const port = process.env.PORT || 4005
 
